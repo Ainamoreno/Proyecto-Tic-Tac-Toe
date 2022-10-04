c82ec1e4604c0e4eb5256975299c6ec3d95dc924
Aina <ainamoreno@outlook.com>
2022-10-04 20:58:56 +0200

    Nombro constantes sobre información de jugadores

--- js/main.js
+++ js/main.js
@@ -1,2 +1,10 @@
+// Constantes información del jugador
+
 const nombreJugadorUno = document.getElementById('name-player1');
 const nombreJugadorDos = document.getElementById('name-player2');
+
+const opcionHumanoJ1 = document.getElementById('player1-option1');
+const opcionCpuJ1 = document.getElementById('player1-option2');
+const opcionHumanoJ2 = document.getElementById('player2-option1');
+const opcionCpuJ2 = document.getElementById('player2-option2');
+

9e38effcfed173aaa801e43d3b85c5de4a43c2cd
Aina <ainamoreno@outlook.com>
2022-10-05 18:08:32 +0200

    Función datos de los jugadores

--- js/main.js
+++ js/main.js
@@ -4,7 +4,79 @@ const nombreJugadorUno = document.getElementById('name-player1');
 const nombreJugadorDos = document.getElementById('name-player2');
 
 const opcionHumanoJ1 = document.getElementById('player1-option1');
-const opcionCpuJ1 = document.getElementById('player1-option2');
 const opcionHumanoJ2 = document.getElementById('player2-option1');
-const opcionCpuJ2 = document.getElementById('player2-option2');
 
+console.log(nombreJugadorUno)
+console.log(opcionHumanoJ2.checked)
+
+
+
+// Objetos
+
+const celdas = [
+    {
+        posicion: 1,
+        seleccionada: false,
+        seleccionadaPor: undefined
+    },
+    {
+        posicion: 2,
+        seleccionada: false,
+        seleccionadaPor: undefined
+    },
+    {
+        posicion: 3,
+        seleccionada: false,
+        seleccionadaPor: undefined
+    },
+    {
+        posicion: 4,
+        seleccionada: false,
+        seleccionadaPor: undefined
+    },
+    {
+        posicion: 5,
+        seleccionada: false,
+        seleccionadaPor: undefined
+    },
+    {
+        posicion: 6,
+        seleccionada: false,
+        seleccionadaPor: undefined
+    },
+    {
+        posicion: 7,
+        seleccionada: false,
+        seleccionadaPor: undefined
+    },
+    {
+        posicion: 8,
+        seleccionada: false,
+        seleccionadaPor: undefined
+    },
+    {
+        posicion: 9,
+        seleccionada: false,
+        seleccionadaPor: undefined
+    },
+]
+// Función datos jugador
+function mostrarDatosJugador() {
+    listaJugadores = JSON.parse(localStorage.getItem('LISTA_JUGADORES'));
+    
+
+    listaJugadores = [
+        {
+            nombre: nombreJugadorUno.value,
+            humano: opcionHumanoJ1.checked
+        },
+        {
+            nombre: nombreJugadorDos.value,
+            humano: opcionHumanoJ2.checked
+        }
+    ]
+
+    localStorage.setItem('LISTA_JUGADORES', JSON.stringify(listaJugadores));
+
+    console.log(listaJugadores[0].nombre)
+}

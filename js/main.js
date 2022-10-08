52d657efb19b7bf4474ed4f730bf997c9bcf22be
Aina <ainamoreno@outlook.com>
2022-10-08 20:21:08 +0200

    Función de mostrar ficha

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
+// Divs | Párrafos de la partida
+const parrafoTurno = document.getElementById('turno-de');
+
 // Constantes información del jugador
 
 const nombreJugadorUno = document.getElementById('name-player1');
@@ -5,11 +8,7 @@ const nombreJugadorDos = document.getElementById('name-player2');
 
 const opcionHumanoJ1 = document.getElementById('player1-option1');
 const opcionHumanoJ2 = document.getElementById('player2-option1');
-
-console.log(nombreJugadorUno)
-console.log(opcionHumanoJ2.checked)
-
-
+let turno = 1;
 
 // Objetos
 
@@ -60,23 +59,65 @@ const celdas = [
         seleccionadaPor: undefined
     },
 ]
+
 // Función datos jugador
-function mostrarDatosJugador() {
-    listaJugadores = JSON.parse(localStorage.getItem('LISTA_JUGADORES'));
-    
+const mostrarDatosJugador = () => {
+    if (nombreJugadorUno.value == "" || nombreJugadorDos.value == "") {
+        alert('Vuelva a la página anterior e introduce datos válidos');
+    } else {
+        listaJugadores = JSON.parse(localStorage.getItem('LISTA_JUGADORES'));
 
-    listaJugadores = [
-        {
-            nombre: nombreJugadorUno.value,
-            humano: opcionHumanoJ1.checked
-        },
-        {
-            nombre: nombreJugadorDos.value,
-            humano: opcionHumanoJ2.checked
-        }
-    ]
-
-    localStorage.setItem('LISTA_JUGADORES', JSON.stringify(listaJugadores));
-
-    console.log(listaJugadores[0].nombre)
+        listaJugadores = [
+            {
+                nombre: nombreJugadorUno.value,
+                humano: opcionHumanoJ1.checked
+            },
+            {
+                nombre: nombreJugadorDos.value,
+                humano: opcionHumanoJ2.checked
+            }
+        ]
+        localStorage.setItem('LISTA_JUGADORES', JSON.stringify(listaJugadores));
+        window.location.href = "start.html";
+    }
 }
+
+const clickFicha = (posicion) => {
+    let arrayTurno = celdas.filter(celda => celda.seleccionadaPor == 1 || celda.seleccionadaPor == 2);
+    let celda = celdas.find(celda => celda.posicion == posicion);
+
+    if (celda.seleccionada == false) {
+
+        if (arrayTurno.length <= 5) {
+            celda.seleccionada = true;
+            celda.seleccionadaPor = turno;
+            mostrarOcultarFicha('block', posicion);
+            turno = turno === 1 ? 2 : 1;
+        } else {
+            alert('No puede añadir ninguna ficha más');  
+        }
+
+    } else if (arrayTurno.length >= 6 && celda.seleccionadaPor == turno) {
+        mostrarOcultarFicha('none', posicion);
+        celda.seleccionada = false;
+        celda.seleccionadaPor = undefined;
+    }
+    console.log(arrayTurno.length);
+};
+
+mostrarOcultarFicha = (display, posicion) => {
+    if (turno == 1) {
+        const cruz = document.getElementById(`icon${posicion}-x`);
+        cruz.style.display = display;
+    } else {
+        const circulo = document.getElementById(`icon${posicion}-o`);
+        circulo.style.display = display;
+    }
+};
+
+const mostrarInformacion = () => {
+    const nombre = JSON.parse(localStorage.getItem("LISTA_JUGADORES"))[0].nombre;
+    parrafoTurno.innerHTML += `<p>Turno de ${nombre}</p>`;
+}
+
+mostrarInformacion()

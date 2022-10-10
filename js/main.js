b8d3c6a07e090f50d2657c075911f83a540296f1
Aina <ainamoreno@outlook.com>
2022-10-10 20:29:42 +0200

    Casuística en caso de ser CPU

--- js/main.js
+++ js/main.js
@@ -65,8 +65,6 @@ const showPlayerData = () => {
     if (namePlayer1.value == "" || namePlayer2.value == "") {
         alert('Vuelva a la página anterior e introduce datos válidos');
     } else {
-        playersList = JSON.parse(sessionStorage.getItem('LISTA_JUGADORES'));
-
         playersList = [
             {
                 name: namePlayer1.value,
@@ -82,8 +80,13 @@ const showPlayerData = () => {
     }
 };
 
+const humanJ1 = JSON.parse(sessionStorage.getItem("LISTA_JUGADORES"))[0].human;
+const humanJ2 = JSON.parse(sessionStorage.getItem("LISTA_JUGADORES"))[1].human;
+
+
+
 const clickIcon = (position) => {
-    let arrayTurn = celdas.filter(cell => cell.selectedFor == 1 || cell.selectedFor == 2);
+    let arrayTurn = cells.filter(cell => cell.selectedFor == 1 || cell.selectedFor == 2);
     let cell = cells.find(cell => cell.position == position);
 
     // casuítica de que entre en el if en caso de que la celda no esté seleccionada
@@ -104,33 +107,74 @@ const clickIcon = (position) => {
             validateWinningOptions(1, 5, 9);
 
             turn = turn === 1 ? 2 : 1;
+
         } else {
             alert('No puede añadir ninguna ficha más');
         }
+        /// Casuística de que jugador 2 sea CPU
 
+        if (humanJ2 == false && turn == 2) {
+            const arrayJ2 = cells.filter(cell => cell.selectedFor == 2);
+            console.log(arrayJ2)
+            const arrayFree = cells.filter(cell => cell.selected == false);
+            let numRandom = Math.floor(Math.random() * arrayFree.length);
+            arrayFree[numRandom].selected = true;
+            arrayFree[numRandom].selectedFor = turn;
+            positionRandom = arrayFree[numRandom].position
+            showHideIcon('block', positionRandom)
+
+
+
+            if (arrayJ2.length >= 3) {
+                const arrayBusy = cells.filter(cell => cell.selected == true && cell.selectedFor == 2);
+                console.log(arrayBusy)
+                let numRandom = Math.floor(Math.random() * arrayBusy.length);
+                positionRandom = arrayBusy[numRandom].position
+                console.log(positionRandom)
+                showHideIcon('none', positionRandom);
+                arrayBusy[numRandom].selected = false;
+                arrayBusy[numRandom].selectedFor = undefined;
+
+                console.log(cells)
+            }
+            validateWinningOptions(1, 2, 3);
+            validateWinningOptions(4, 5, 6);
+            validateWinningOptions(7, 8, 9);
+            validateWinningOptions(3, 6, 9);
+            validateWinningOptions(2, 5, 8);
+            validateWinningOptions(1, 4, 7);
+            validateWinningOptions(3, 5, 7);
+            validateWinningOptions(1, 5, 9);
+            console.log(turn)
+            turn = turn === 1 ? 2 : 1;
+
+        };
         // casuítica de que estén 6 celdas seleccionadas y se de click a una celda que le pertenezca al jugador
     } else if (arrayTurn.length >= 6 && cell.selectedFor == turn) {
         showHideIcon('none', position);
         cell.selected = false;
         cell.selectedFor = undefined;
     }
-
-
-    console.log(cell)
     showInformation()
-    console.log(arrayTurn)
 };
 
+
+// Mostrar u ocultar ficha
 showHideIcon = (display, position) => {
-    if (turn == 1) {
-        const cross = document.getElementById(`icon${position}-x`);
-        cross.style.display = display;
-    } else {
+    if (turn == 2 && humanJ2 == false) {
         const circle = document.getElementById(`icon${position}-o`);
         circle.style.display = display;
+
+    } else if (turn == 2 && humanJ2 == true) {
+        const circle = document.getElementById(`icon${position}-o`);
+        circle.style.display = display;
+    } else {
+        const cross = document.getElementById(`icon${position}-x`);
+        cross.style.display = display;
     }
 };
 
+// Mostrar información jugador
 const showInformation = () => {
     if (turn == 1) {
         textTurn.innerHTML = " ";
@@ -144,6 +188,7 @@ const showInformation = () => {
 
 };
 
+// Validar opciones ganadoras
 const validateWinningOptions = (pos1, pos2, pos3) => {
     let arrayPlayerTurn = cells.filter(cell => cell.selectedFor == turn);
     if (arrayPlayerTurn.length == 3) {
